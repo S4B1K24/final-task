252caa38af5f8b8d5a508fbd7c853c0b19472b7b
S4B1K <83269819+S4B1K24@users.noreply.github.com>
2023-05-27 13:55:42 +0700

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,11 +1,22 @@
 #!/bin/sh
 
-if [ -f .env ]; then
-    source .env
+if [ "$DATABASE" = "postgres" ]
+then
+    echo "Waiting for postgres..."
+
+    while ! nc -z $SQL_HOST $SQL_PORT; do
+      sleep 0.1
+    done
+
+    echo "PostgreSQL started"
 fi
 
-if [ $1 = "gunicorn" ]; then
-    poetry run gunicorn -b 0.0.0.0:8000 sampleproject.wsgi
-else
-    poetry run python manage.py $@
+if [ $# = 1 ]; then
+    if [ $1 = "test" ]; then
+      python -m unittest tests.py
+    fi
 fi
+
+python manage.py create_db
+
+exec "$@
