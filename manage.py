b858dd1d9347a2b8ad805c7ce1641cf24db06d7b
S4B1K <83269819+S4B1K24@users.noreply.github.com>
2023-05-27 14:13:24 +0700

    Update manage.py

--- manage.py
+++ manage.py
@@ -1,11 +1,12 @@
 #!/usr/bin/env python
 import os
 import sys
-import django
+from django import models
+from django.core.management import execute_from_command_line
 
 if __name__ == "__main__":
     os.environ.setdefault("DJANGO_SETTINGS_MODULE", "sampleproject.settings")
 
-    from django.core.management import execute_from_command_line
+    
 
     execute_from_command_line(sys.argv)
