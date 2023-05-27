828552e8dd3e883cab84dae99bc05d6bc9ab932b
S4B1K <83269819+S4B1K24@users.noreply.github.com>
2023-05-27 15:34:48 +0700

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,10 +1,10 @@
-#!/bin/bash
+#!/bin/sh
 
 source venv/bin/activate
-pip install -r requirements.txt
 
 if [ $1 = "gunicorn" ]; then
     poetry run gunicorn -b 0.0.0.0:8000 sampleproject.wsgi
 else
     poetry run python manage.py $@
 fi
+exec $@
