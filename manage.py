65ffa10d8a6070e3a61c946c55699f9908d8215d
S4B1K <83269819+S4B1K24@users.noreply.github.com>
2023-05-27 15:07:21 +0700

    Update manage.py

--- manage.py
+++ manage.py
@@ -5,7 +5,6 @@ import sys
 if __name__ == "__main__":
     os.environ.setdefault("DJANGO_SETTINGS_MODULE", "sampleproject.settings")
     
-    pip install Django 
-    from django.core.management import execute_from_command_line
+    from Django.core.management import execute_from_command_line
 
     execute_from_command_line(sys.argv)
