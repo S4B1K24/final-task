5fd86b9caa60ac2a0dae0da0738b4c4146ca2f8c
S4B1K <83269819+S4B1K24@users.noreply.github.com>
2023-06-01 18:14:20 +0700

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,3 +1,11 @@
-#!/bin/bash
-. /appenv/bin/activate
-exec $@
+#!/bin/sh
+
+if [ -f .venv ]; then
+    source .venv
+fi
+
+if [ $1 = "gunicorn" ]; then
+    poetry run gunicorn -b 0.0.0.0:8000 sampleproject.wsgi
+else
+    poetry run python manage.py $@
+fi
